6627472127d85771be5e924e2f02361ec0908ea7
rjthapaa <127729631+rjthapaa@users.noreply.github.com>
2023-09-03 17:15:35 +0530

    Create deployagent.sh

---
+++ deployagent.sh
@@ -0,0 +1,10 @@
+#Linux Deployagent installation steps
+#!/bin/bash
+sudo yum update
+sudo yum install ruby
+sudo yum install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
